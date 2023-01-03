93dcb78ea76bc222d58e7dff19cac31ca1a4d9e2
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2023-01-03 15:15:15 -0300

    Updates to factorial.py

--- python/factorial.py
+++ python/factorial.py
@@ -1,17 +1,27 @@
-def factorial(n):
-  # Base case: n = 0
-  if n == 0:
-    return 1
-  
-  # Recursive case: n > 0
-  else:
-    return n * factorial(n - 1)
+def factorial(n, memo={}):
+    # Base case: n = 0
+    if n == 0:
+        return 1
+
+    # Check if the factorial has already been calculated
+    if n in memo:
+        return memo[n]
+
+    # Recursive case: n > 0
+    else:
+        result = n * factorial(n - 1, memo)
+        memo[n] = result
+        return result
 
 # Test the function
-print(factorial(5)) # Should print 120
-print(factorial(0)) # Should print 1
-print(factorial(1)) # Should print 1
-print(factorial(10)) # Should print 3628800
+n = int(input("Enter a positive integer: "))
+print(factorial(n))
+
+# Test the function
+#print(factorial(5)) # Should print 120
+#print(factorial(0)) # Should print 1
+#print(factorial(1)) # Should print 1
+#print(factorial(10)) # Should print 3628800
 
 # We defines a function called factorial that calculates the factorial
 # of a given number n. The factorial of a number is the product of all
@@ -21,3 +31,9 @@ print(factorial(10)) # Should print 3628800
 # The factorial function uses recursion to calculate the factorial. It has two cases:
 ## If n is 0, the function returns 1 (the base case).
 ## If n is greater than 0, the function returns n multiplied by the factorial of n - 1.
+#
+# Update: this now uses a dictionary called memo to store the results of previously
+# calculated factorials. If the factorial of a given number has already been calculated,
+# it is retrieved from the memo dictionary and returned. If it has not been calculated yet,
+# it is calculated using the recursive case and then stored in the memo dictionary for
+# future reference. 
